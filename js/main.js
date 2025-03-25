791a56aef1074c9619d6b605e1df545ae84c9c8e
Ivan Aranda <96434524+TheNaviam@users.noreply.github.com>
2025-03-25 22:08:50 +0100

    pagina auxiliar de galeria terminada

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-jQuery(function($) {
+jQuery(function ($) {
 
-	$(function(){
+	$(function () {
 		$('#main-slider.carousel').carousel({
 			interval: 10000,
 			pause: false
@@ -11,29 +11,39 @@ jQuery(function($) {
 	var form = $('.contact-form');
 	form.submit(function () {
 		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
+		$.post($(this).attr('action'), function (data) {
 			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
+		}, 'json');
 		return false;
 	});
 
-	//smooth scroll
-	$('.navbar-nav > li').click(function(event) {
-		event.preventDefault();
-		var target = $(this).find('>a').prop('hash');
-		$('html, body').animate({
-			scrollTop: $(target).offset().top
-		}, 500);
+	// Detectar si la página actual es index.html
+	function esIndex() {
+		return window.location.pathname.endsWith("index.html") || window.location.pathname === "/web/" || window.location.pathname === "/web/index.html";
+	}
+
+	// Smooth scroll (solo en index.html)
+	$('.navbar-nav > li').click(function (event) {
+		var target = $(this).find('>a').prop('hash'); // Obtiene el hash (#services, #portfolio, etc.)
+
+		if (esIndex() && target && $(target).length) {
+			event.preventDefault(); // SOLO bloquea la navegación en index.html
+			$('html, body').animate({
+				scrollTop: $(target).offset().top
+			}, 500);
+		} else {
+			window.location.href = "/web/index.html" + target; // Redirige a index.html si no está en él
+		}
 	});
 
 	//cerrar navar
 	$(document).ready(function () {
-        $(".navbar-nav li a").click(function () {
-            if ($(".navbar-toggle").is(":visible")) {
-                $(".navbar-collapse").collapse("hide");
-            }
-        });
-    });
+		$(".navbar-nav li a").click(function () {
+			if ($(".navbar-toggle").is(":visible")) {
+				$(".navbar-collapse").collapse("hide");
+			}
+		});
+	});
 
 	//scrollspy
 	$('[data-spy="scroll"]').each(function () {
@@ -46,14 +56,14 @@ jQuery(function($) {
 	});
 
 	//Isotope
-	$(window).load(function(){
+	$(window).load(function () {
 		$portfolio = $('.portfolio-items');
 		$portfolio.isotope({
-			itemSelector : 'li',
-			layoutMode : 'fitRows'
+			itemSelector: 'li',
+			layoutMode: 'fitRows'
 		});
 		$portfolio_selectors = $('.portfolio-filter >li>a');
-		$portfolio_selectors.on('click', function(){
+		$portfolio_selectors.on('click', function () {
 			$portfolio_selectors.removeClass('active');
 			$(this).addClass('active');
 			var selector = $(this).attr('data-filter');
